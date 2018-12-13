b6b34391575afa5422972f5ad1e7f86fd23d9a98
XNYu <yyhh8375@gmail.com>
2018-12-12 23:24:29 -0500

    Add

---
+++ src/LC486PredictWinner.java
@@ -0,0 +1,31 @@
+public class LC486PredictWinner {
+    public boolean PredictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) { dp[i][i] = nums[i]; }
+        for (int len = 1; len < n; len++) {
+            for (int i = 0; i < n - len; i++) {
+                int j = i + len;
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1] >= 0;
+    }
+    // O(N) space
+    public boolean PredictTheWinner2(int[] nums) {
+        if (nums == null) { return true; }
+        int n = nums.length;
+        if ((n & 1) == 0) { return true; } // Improved with hot13399's comment.
+        int[] dp = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (i == j) {
+                    dp[i] = nums[i];
+                } else {
+                    dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+                }
+            }
+        }
+        return dp[n - 1] >= 0;
+    }
+}
